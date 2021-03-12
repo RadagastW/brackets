7505ee0e62eb0c83a78533af7d449b7d9ce45d61
RadagastW <radagastw@yandex.ru>
2021-03-12 19:10:29 +0600

    feat: inplement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsArr = bracketsConfig.map(item => item.join(''));
+    let edStr = str;
+    let result = false;
+
+    let index = 0;
+    while (index < bracketsArr.length) {
+        if (edStr.includes(bracketsArr[index])) {
+            edStr = edStr.replace(bracketsArr[index], '');
+            index = -1;
+        }
+        index++;
+    }
+
+    if (edStr == '') result = true;
+
+    return result;
 }
